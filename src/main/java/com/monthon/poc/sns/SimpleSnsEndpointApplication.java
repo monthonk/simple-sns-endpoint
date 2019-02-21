6c51f9cb127331d4b7415096c541e16501335a55
Monthon Klongklaw <monthon.klo@ascendcorp.com>
2019-02-21 17:41:08 +0700

    initial project

---
+++ src/main/java/com/monthon/poc/sns/SimpleSnsEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.monthon.poc.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSnsEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSnsEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monthon/poc/sns/SimpleSnsEndpointApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monthon.poc.sns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSnsEndpointApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
